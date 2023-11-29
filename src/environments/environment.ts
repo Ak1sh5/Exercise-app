73f21d49f8526dca0e0347407baa9ca079db41af
Akash <startedcoding04062022@gmail.com>
2023-11-29 18:05:51 +0530

    Version 1 with deployment ready

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
     production: false,
-    apiKey: 'fc0651007bmshea3172ea513eadcp118aebjsndf485bc99a6e',
+    apiKey: '',
     apiHost: 'exercises-by-api-ninjas.p.rapidapi.com',
     host:'exercises-by-api-ninjas.p.rapidapi.com'
   };
